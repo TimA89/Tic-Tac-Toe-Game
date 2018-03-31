04e7b38adb6c61a4ea6feb760799418650ee670a
TimA89 <timabuladze@gmail.com>
2018-03-30 23:07:05 -0400

    Added api
    cant set it to change a password yet

---
+++ assets/scripts/api.js
@@ -0,0 +1,49 @@
+const config = require('./config')
+
+const store = require('./store')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+const signOut = function () {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out/' + store.user.id,
+    method: 'DELETE',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const changePW = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password/' + store.user.id,
+    method: 'PATCH',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePW
+}
